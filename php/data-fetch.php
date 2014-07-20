c14f86164ed1f17d1bf197940b71d8f41040e819
Kunal Shah <kunalshah@fb.com>
2014-07-19 18:26:17 -0700

    Updating with new files

--- php/data-fetch.php
+++ php/data-fetch.php
@@ -1,40 +1,96 @@
 <?php
-$username = "unochaviz"; 
-$password = "4fU8xNjMDcEK";   
-$host = "unocha.ck9lcpfkyytw.us-east-1.rds.amazonaws.com";
-$database="unocha";
+function fetchData($operation) {
 
-$connection = pg_connect("host=$host port=5432 dbname=$database user=$username password=$password") or die('Cannot connect to host:');
+	$username = "unochaviz"; 
+	$password = "4fU8xNjMDcEK";   
+	$host = "unocha.ck9lcpfkyytw.us-east-1.rds.amazonaws.com";
+	$database="unocha";
 
-$origin = $_GET['origin'];
-$residence = $_GET['residence'];
+	$connection = pg_connect("host=$host port=5432 dbname=$database user=$username password=$password") or die('Cannot connect to host:');
 
-$query = 'SELECT * from asylum_flows';
-$clauses = array();
+	$origin = $_GET['origin'];
+	$residence = $_GET['residence'];
+	switch ($operation) {
+		case 'asylum':
+			$query = fetchAsylumData($origin, $residence);
+			break;	
+		case 'migration': 
+			$query = fetchMigrationData($origin, $residence);
+			break;
+		case 'indicator':
+			$query = fetchIndicatorData($origin);
+			break;
+		default:
+			exit();
+	}
 
-if ($residence) {
-	$clauses[] = "residence = '$residence'";
+	$result = pg_query($connection, $query);
+
+	if (!$result) {
+		echo "Problem with query " . $query . "<br/>";
+		echo pg_last_error(); 
+		exit(); 
+	}
+	$data = array();
+	while($myrow = pg_fetch_assoc($result)) {
+		$data[] = $myrow; 
+	}
+
+	$data = json_encode($data);
+	echo $data;
+
 }
 
-if ($origin) {
-	$clauses[] = "origin = '$origin'";
-}
-if ($clauses) {
-	$query .= ' WHERE '.implode(' AND ',$clauses);
+/**
+ * Returns query for fetching asylum data for specified origin and residence country.
+ */
+function fetchAsylumData($origin, $residence) {
+	$query = 'SELECT * FROM asylum_flows WHERE ';
+	$clauses = array();
+
+	if ($residence) {
+		$clauses[] = "residence = '$residence'";
+	}
+
+	if ($origin) {
+		$clauses[] = "origin = '$origin'";
+	}
+	if ($clauses) {
+		$query .= implode(' AND ',$clauses);
+	}
+	
+	return $query;
 }
 
-$result = pg_query($connection, $query);
+/**
+ * Returns query for fetching migration data for specified origin and residence country.
+ */
+function fetchMigrationData($origin, $residence) {
+	$query = 'SELECT * FROM asylum_flows WHERE';
+	$clauses = array();
 
-if (!$result) {
-	echo "Problem with query " . $query . "<br/>";
-	echo pg_last_error(); 
-	exit(); 
-}
-$data = array();
-while($myrow = pg_fetch_assoc($result)) {
-	$data[] = $myrow; 
+	if ($residence) {
+		$clauses[] = "country = '$residence'";
+	}
+
+	if ($origin) {
+		$clauses[] = "country_of_origin = '$origin'";
+	}
+	if ($clauses) {
+		$query .= implode(' AND ',$clauses);
+	}
+	
+	return $query;
 }
 
-$data = json_encode($data);
-echo $data;
+/**
+ * Returns query for fetching indicator data for country.
+ */
+function fetchIndicatorData($country) {
+
+}
+
+
+$operation = $_GET['operation'];
+fetchData($operation);
 ?>
