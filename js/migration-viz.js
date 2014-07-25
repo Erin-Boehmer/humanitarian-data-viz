fbc9db8c2f73810804af72941db5edab9dcd1cc9
Erin <elb9fs@virginia.edu>
2014-07-25 03:04:11 -0400

    Support for zoom integrated with tooltip & flow arcs

--- js/migration-viz.js
+++ js/migration-viz.js
@@ -1,78 +1,169 @@
-/* migration-viz.js */
-
 function init() {
 
-// Create the canvas
-var svg = d3.select('#map').append('svg')
-    .attr('width', '100%')
-    .attr('height', '100%');
+d3.select(window).on("resize", throttle);
 
-// Choose the projection.
-var projection = d3.geo.mercator();
+var zoom = d3.behavior.zoom()
+    .scaleExtent([1, 9])
+    .on("zoom", move);
 
-// Set the created projection on the path.
-var path = d3.geo.path()
-    .projection(projection)
-var unprojectedPath = d3.geo.path();
+var width = document.getElementById('container').offsetWidth;
+var height = width / 2;
+
+var topo,projection,path,svg,g;
+
+var nameToFeatureMap = Array();
+
+var tooltip = d3.select("#container").append("div").attr("class", "tooltip hidden");
+var flow_tooltip = d3.select("#container").append("div").attr("class", "tooltip hidden");
+
+setup(width,height);
 
 // Create the arc creator function
 var arc = d3.geo.greatArc().precision(1);
 
-// Store the map from country name to country code inside a variable map.
-var nameToFeatureMap = Array();
-$.getJSON(
-'ne_10m_admin_0_countries_lakes/countries.topo.json',
-function (countries) {
-features = topojson.feature(countries, countries.objects.countries).features;
-for (var i in features) {
-feature = features[i];
-nameToFeatureMap[feature.properties.NAME] = feature;
-}
-}
-);
+function setup(width,height){
+  projection = d3.geo.mercator()
+    .translate([(width/2), (height/2)])
+    .scale( width / 2 / Math.PI);
 
-// Use the topojson file to render the map.
-d3.json('ne_10m_admin_0_countries_lakes/countries.topo.json', function(error, countries) {
-    svg.selectAll('.countries')
-        .data(topojson.feature(countries, countries.objects.countries).features)
-.enter()
-.append('path')
-        .attr('class', 'countries')
-.attr('id', function (d) {return d.id;})
-        .attr('d', path)
-.on('mouseover', function() {
-d3.select(this).classed('hover', true);
-})
-.on('mouseout', function() {
-d3.select(this).classed('hover', false);
-})
-.on('click', plotFlows)
-.append('title').text(function(d) {return d.properties.NAME;});
+  path = d3.geo.path().projection(projection);
+
+  svg = d3.select("#container").append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .call(zoom)
+      .on("click", click)
+      .append("g");
+
+  g = svg.append("g");
+
+}
+
+d3.json("data/world-topo-min.json", function(error, world) {
+
+  var countries = topojson.feature(world, world.objects.countries).features;
+  topo = countries;
+  draw(topo);
+	
+  // Populate nameToFeatureMap for plotting arcs convenience
+  for (var i in countries) {
+	feature = countries[i];
+	nameToFeatureMap[feature.properties.name] = feature;
+  }
+
 });
 
-/**
-* Input: TopoJSON Feature
-* Result: Plots arcs from source country to affected country.
-*/
+
+
+function draw(topo) {
+
+  var country = g.selectAll(".country").data(topo);
+  country.enter().insert("path")
+      .attr("class", "country")
+      .attr("d", path)
+      .attr("id", function(d,i) { return d.id; })
+      .attr("title", function(d,i) { return d.properties.name; })
+      .style("fill", function(d, i) { return d.properties.color; });
+
+
+  d3.csv("data/countries-lat-long.csv", function(error, latlong) {
+	// NEED TO DO THIS STILL - FIND COUNTRY LAT LONG RATHER THAN CENTROID
+
+  });
+  
+  //offsets for tooltips
+  var offsetL = document.getElementById('container').offsetLeft+20;
+  var offsetT = document.getElementById('container').offsetTop+10;
+
+  //tooltips
+  country
+    .on("mousemove", function(d,i) {
+    	var mouse = d3.mouse(svg.node()).map( function(d) { return parseInt(d); } );
+	tooltip.classed("hidden", false)
+        	.attr("style", "left:"+(mouse[0]+offsetL)+"px;top:"+(mouse[1]+offsetT)+"px")
+             	.html(d.properties.name);
+    })
+    .on("mouseout",  function(d,i) {
+        tooltip.classed("hidden", true);
+    })
+    .on("click", plotFlows); 
+
+
+}
+
+
+function redraw() {
+  width = document.getElementById('container').offsetWidth;
+  height = width / 2;
+  d3.select('svg').remove();
+  setup(width,height);
+  draw(topo);
+}
+
+
+function move() {
+
+  var t = d3.event.translate;
+  var s = d3.event.scale; 
+
+  zscale = s;
+  var h = height/4;
+
+
+  t[0] = Math.min(
+    (width/height)  * (s - 1), 
+    Math.max( width * (1 - s), t[0] )
+  );
+
+  t[1] = Math.min(
+    h * (s - 1) + h * s, 
+    Math.max(height  * (1 - s) - h * s, t[1])
+  );
+
+  zoom.translate(t);
+  g.attr("transform", "translate(" + t + ")scale(" + s + ")");
+  
+  //adjust the country hover stroke width based on zoom level
+  d3.selectAll(".country").style("stroke-width", 1.5 / s);
+
+}
+
+
+
+
+var throttleTimer;
+function throttle() {
+  window.clearTimeout(throttleTimer);
+    throttleTimer = window.setTimeout(function() {
+      redraw();
+    }, 200);
+}
+
+
+//geo translation on mouse click in map
+function click() {
+  var latlon = projection.invert(d3.mouse(this));
+  console.log(latlon);
+}
+
+//function to plot migration flows on country click
 function plotFlows(data) {
-// Remove current selection, and mark clicked country as selected.
-d3.select('.selected').classed('selected', false);
-d3.select('#'+data.id).classed('selected', true);
+  // Remove existing flow arcs
+  d3.selectAll(".flow").remove();
 
-// Fetch the data, and run two different callbacks. One callback should
-// color the countries, and the second should render the aggregate graph.
-$.ajax({
-type: "GET",
-url: "php/data-fetch.php",
-data: {
-"operation": 'asylum',
-"origin": data.properties.NAME
-},
-dataType: "json",
-success: function (migration_data) {
-plotLines(data.properties.NAME, migration_data);
-}
-});
+  // Fetch the data and call plotLines() on callback
+  $.ajax({
+	type: "GET",
+	 url: "php/data-fetch.php",
+	 data: {
+  		"operation": 'asylum',
+		"origin": data.properties.name
+	},
+	dataType: "json",
+	success: function (migration_data) {
+		plotLines(data.properties.name, migration_data);
+	}
+  });
 }
 
 /**
@@ -80,36 +171,118 @@ plotLines(data.properties.NAME, migration_data);
 * Result: Plots arcs from source country to every target country.
 */
 function plotLines(sourceCountry, migrationData) {
-	svg.selectAll('.arcs').remove();	
 	var aggregateByTargetCountry = reduce(migrationData, 'residence', 'applied_during_year');
-	console.log(aggregateByTargetCountry);	
-	// Get the location of the source country.
-	var source = projection.invert(path.centroid(nameToFeatureMap[sourceCountry]));
-	console.log(nameToFeatureMap);
-	
-	max = Math.log(d3.max(d3.values(aggregateByTargetCountry)));
+  
+  	// Get the location of the source country.
+  	var source = projection.invert(path.centroid(nameToFeatureMap[sourceCountry]));
+	var max = Math.log(d3.max(d3.values(aggregateByTargetCountry)));
 	for (var targetCountry in aggregateByTargetCountry) {
+		//d3.selectAll("[title="+targetCountry+"]").style("fill","#000");
 		var target = projection.invert(path.centroid(nameToFeatureMap[targetCountry]));
 		link = {'source': source, 'target': target};
 		dValue = path(arc(link));
 
-		strokeValue = Math.log(aggregateByTargetCountry[targetCountry]+1)/max;
+		var strokeValue = Math.log(aggregateByTargetCountry[targetCountry]+1)/max;
 		strokeValue = 0.5 + 10*strokeValue;
-		
-		var countryID = 'unknown';
-		if (nameToFeatureMap[targetCountry] != undefined) {
-			countryID = nameToFeatureMap[targetCountry].id;
-		}
-		svg.append("path")
-			.datum(countryID)
+	
+	  	//offsets for tooltips
+  		var offsetL = document.getElementById('container').offsetLeft+20;
+  		var offsetT = document.getElementById('container').offsetTop+10;
+				
+		g.append("path")
+			.datum(targetCountry)
+			.attr("class","flow")
 			.attr("d", dValue)
-			.attr('stroke-width', strokeValue)
-			.attr('class', 'arcs')
-			.attr("marker-end", "url(#arrowHead)")
-			.on("mouseover", function(countryID) {d3.select('#' + countryID).classed('hover', true)})
-			.on("mouseout", function (countryID) {d3.select('#' + countryID).classed('hover', false)})
-			.append('svg:title').text('Country: ' + targetCountry + ", Applicants:"+aggregateByTargetCountry[targetCountry]);
+  			.style('stroke-width', strokeValue)
+    			.on("mouseover", function(d,i) {
+        			var mouse = d3.mouse(svg.node()).map( function(d) { return parseInt(d); } );
+        			flow_tooltip.classed("hidden", false)
+                			.attr("style", "left:"+(mouse[0]+offsetL)+"px;top:"+(mouse[1]+offsetT)+"px")
+                			.html('Country: ' + d + ", Applicants: " + aggregateByTargetCountry[d]);
+    			})
+    			.on("mouseout",  function(d,i) {
+        			flow_tooltip.classed("hidden", true);
+    			})
+
+
 	}
+}
+
+
+function reduce(data, key, value) {
+	var aggregatedData = new Array();
+	for (var i in data) {
+		row = data[i];
+		if (aggregatedData[row[key]] == undefined) {
+			aggregatedData[row[key]] = parseInt(row[value]);
+		} else {
+			aggregatedData[row[key]] += parseInt(row[value]);
+		}
+	}
+	
+	return aggregatedData;
+}
+
+
+
+
+
+/** AFTER THIS POINT, FUNCTIONS NOT CALLED YET **/
+
+
+
+function drawLineChart(data) {	
+	d3.select('#line_graph').remove();
+	// define dimensions of graph
+	var m = [80, 80, 80, 80]; // margins
+	var w = 1000 - m[1] - m[3]; // width
+	var h = 400 - m[0] - m[2]; // height
+
+	// X scale will fit all values from data[] within pixels 0-w
+	var x = d3.scale.linear()
+		.domain([d3.min(d3.keys(data)), d3.max(d3.keys(data))])
+		.range([0, w]);
+
+	// Y scale will fit values from 0-10 within pixels h-0 (Note the inverted domain for the y-scale: bigger is up!)
+	var y = d3.scale.linear()
+		.domain([0,d3.max(d3.values(data))])
+		.range([h, 0]);
+
+	// create a line function that can convert data[] into x and y points
+	var line = d3.svg.line()
+		// assign the X function to plot our line as we wish
+		.x(function(d) { return x(d.key); })
+		.y(function(d) { return y(d.value); });
+
+	// Add an SVG element with the desired dimensions and margin.
+	var graph = d3.select("#graph")
+		.append("svg:svg")
+		.attr("width", w + m[1] + m[3])
+		.attr("height", h + m[0] + m[2])
+		.attr("id", "line_graph")
+		.append("svg:g")
+		.attr("transform", "translate(" + m[3] + "," + m[0] + ")");
+
+	// create yAxis
+	var xAxis = d3.svg.axis().scale(x).tickSize(-h).tickSubdivide(true);
+	// Add the x-axis.
+	graph.append("svg:g")
+		.attr("class", "x axis")
+		.attr("transform", "translate(0," + h + ")")
+		.call(xAxis);
+
+
+	// create left yAxis
+	var yAxisLeft = d3.svg.axis().scale(y).ticks(4).orient("left");
+	// Add the y-axis to the left
+	graph.append("svg:g")
+		.attr("class", "y axis")
+		.attr("transform", "translate(-25,0)")
+		.call(yAxisLeft);
+
+	// Add the line by appending an svg:path element with the data line we created above
+	// do this AFTER the axes above so that the line is above the tick-lines
+	graph.append("svg:path").attr("d", line(d3.entries(data)));
 }
 
 /**
@@ -117,25 +290,25 @@ function plotLines(sourceCountry, migrationData) {
 * Result: Shades target countries by data weight, and plots aggregate for source country.
 */
 function onSelectCountry(data) {
-// Remove current selection, and mark clicked country as selected.
-d3.select('.selected').classed('selected', false);
-d3.select('#'+data.id).classed('selected', true);
+	// Remove current selection, and mark clicked country as selected.
+	d3.select('.selected').classed('selected', false);
+	d3.select('#'+data.id).classed('selected', true);
 
-// Fetch the data, and run two different callbacks. One callback should
-// color the countries, and the second should render the aggregate graph.
-$.ajax({
-type: "GET",
-url: "php/data-fetch.php",
-data: {
-"operation": 'asylum',
-"origin": data.properties.NAME
-},
-dataType: "json",
-success: function (data) {
-colorCountries(data);
-plotAggregateGraph(data);
-}
-});
+	// Fetch the data, and run two different callbacks. One callback should
+	// color the countries, and the second should render the aggregate graph.
+	$.ajax({
+		type: "GET",
+		url: "php/data-fetch.php",
+		data: {
+			"operation": 'asylum',
+			"origin": data.properties.NAME
+		},
+		dataType: "json",
+		success: function (data) {
+			colorCountries(data);
+			plotAggregateGraph(data);
+		}	
+	});
 }
 
 /**
@@ -143,96 +316,24 @@ plotAggregateGraph(data);
 * Result: Colors country by size of value.
 */
 function colorCountries(data) {
-$.find('.countries').forEach(function(elem) { elem.style.removeProperty('fill'); });
-var aggregateByTargetCountry = reduce(data, 'residence', 'applied_during_year');
-var scale = d3.scale
-.linear()
-.domain([0,d3.max(d3.values(aggregateByTargetCountry))])
-.range([0,256]);
-max = Math.log(d3.max(d3.values(aggregateByTargetCountry)));
-for (var country in aggregateByTargetCountry) {
-value = Math.log(aggregateByTargetCountry[country]+1)/max * 64;
-value = parseInt( + value);
-value = 'rgb(256,'+(256-(value*4))+','+(256-(value*4))+')';
+	$.find('.countries').forEach(function(elem) { elem.style.removeProperty('fill'); });
+	var aggregateByTargetCountry = reduce(data, 'residence', 'applied_during_year');
+	var scale = d3.scale
+		.linear()
+		.domain([0,d3.max(d3.values(aggregateByTargetCountry))])
+		.range([0,256]);
+	max = Math.log(d3.max(d3.values(aggregateByTargetCountry)));
+	for (var country in aggregateByTargetCountry) {
+		value = Math.log(aggregateByTargetCountry[country]+1)/max * 64;
+		value = parseInt( + value);
+		value = 'rgb(256,'+(256-(value*4))+','+(256-(value*4))+')';
 
-element = $.find("#"+nameToFeatureMap[country])[0];
-if ( element != undefined) { element.style.setProperty('fill', value); }
-}
+		element = $.find("#"+nameToFeatureMap[country])[0];
+		if ( element != undefined) { element.style.setProperty('fill', value); }
+	}
 
-}
-
-function plotAggregateGraph(data) {
-var aggregateByYear = reduce(data, 'year', 'applied_during_year');
-drawLineChart(aggregateByYear);
-}
-
-function reduce(data, key, value) {
-var aggregatedData = new Array();
-for (var i in data) {
-row = data[i];
-if (aggregatedData[row[key]] == undefined) {
-aggregatedData[row[key]] = parseInt(row[value]);
-} else {
-aggregatedData[row[key]] += parseInt(row[value]);
-}
-}
-return aggregatedData;
 }
 
 
-function drawLineChart(data) {	
-d3.select('#line_graph').remove();
-// define dimensions of graph
-var m = [80, 80, 80, 80]; // margins
-var w = 1000 - m[1] - m[3]; // width
-var h = 400 - m[0] - m[2]; // height
-
-// X scale will fit all values from data[] within pixels 0-w
-var x = d3.scale.
-linear().
-domain([d3.min(d3.keys(data)), d3.max(d3.keys(data))])
-.range([0, w]);
-// Y scale will fit values from 0-10 within pixels h-0 (Note the inverted domain for the y-scale: bigger is up!)
-var y = d3.scale.
-linear().
-domain([0,d3.max(d3.values(data))])
-.range([h, 0]);
-
-// create a line function that can convert data[] into x and y points
-var line = d3.svg.line()
-// assign the X function to plot our line as we wish
-.x(function(d) { return x(d.key); })
-.y(function(d) { return y(d.value); });
-
-// Add an SVG element with the desired dimensions and margin.
-var graph = d3.select("#graph")
-.append("svg:svg")
-.attr("width", w + m[1] + m[3])
-.attr("height", h + m[0] + m[2])
-.attr("id", "line_graph")
-.append("svg:g")
-.attr("transform", "translate(" + m[3] + "," + m[0] + ")");
-
-// create yAxis
-var xAxis = d3.svg.axis().scale(x).tickSize(-h).tickSubdivide(true);
-// Add the x-axis.
-graph.append("svg:g")
-.attr("class", "x axis")
-.attr("transform", "translate(0," + h + ")")
-.call(xAxis);
-
-
-// create left yAxis
-var yAxisLeft = d3.svg.axis().scale(y).ticks(4).orient("left");
-// Add the y-axis to the left
-graph.append("svg:g")
-.attr("class", "y axis")
-.attr("transform", "translate(-25,0)")
-.call(yAxisLeft);
-
-// Add the line by appending an svg:path element with the data line we created above
-// do this AFTER the axes above so that the line is above the tick-lines
-graph.append("svg:path").attr("d", line(d3.entries(data)));
-}
 
 }
