9194e0c3ba9d06713646a1d5c6b8892e834fb00c
Erin-Boehmer <erin.boehmer@gmail.com>
2014-08-20 05:39:21 -0400

    Finished the Zimbabwe story display

--- js/migration/controllers.js
+++ js/migration/controllers.js
@@ -127,7 +127,10 @@ angular.module('myApp.controllers', ['ui.bootstrap'])
     ])
     .controller('ZimbabweController', ['$scope',
         function($scope) {
-         $scope.operation = 'asylum_from';
+         
+	$scope.panelToggleClass = "btn glyphicon glyphicon-chevron-up";
+
+	$scope.operation = 'asylum_from';
          var migration = viz.init(function() {
                 migration.selectCountry('Zimbabwe', 'asylum_from')
          }, true)
@@ -153,22 +156,45 @@ angular.module('myApp.controllers', ['ui.bootstrap'])
 			if(element.count > 0) 
 				$scope.totalApplicants += element.count;
 		});
-		console.log($scope.totalApplicants);
 	});
 
-	//Set functions required in "ExploreController" to do nothing
         $scope.resetCountries = function() {
 		$scope.countries = []
 	}
+	
+	//Set function required in "ExploreController" to do nothing
         $scope.selectCountry = function(country) {}
+	$scope.indicatorChanged = function(){}
 
        	$scope.year = 2003;
 
        	$scope.yearChanged = function(){
 		migration.onSelectOperation();
-                console.log($scope.year);
                 $scope.indicatorChanged();
         }
+
+	$scope.controlPanelToggle = function() {
+		$('#control-panel-body').toggle();
+                if($scope.panelToggleClass == "btn glyphicon glyphicon-chevron-up") {
+                        $scope.panelToggleClass = "btn glyphicon glyphicon-chevron-down";
+                } else {
+                        $scope.panelToggleClass = "btn glyphicon glyphicon-chevron-up";
+                }
+	}
+
+	$scope.story = {"2003":{"imglink":"img/zimbabwe/2003.gif","text":"Under President Robert Mugabe, Zimbabwe withdraws from the Commonwealth of Nations. Black squatters continue to seize white-owned farms in a violent campaign to reclaim what they say was stolen by settlers, exacerbating food shortages and threatening famine."},
+			"2004":{"imglink":"img/zimbabwe/2004.jpg","text":"Opposition leader Morgan Tsvangirai is acquitted of treason charges relating to an alleged plot to kill President Mugabe. He faces a separate treason charge."},
+			"2005":{"imglink":"img/zimbabwe/2005.jpg","text":"The US labels Zimbabwe as one of the world's six 'outposts of tyranny' while UN humanitarian chief says Zimbabwe is in 'meltdown.' Zimbabwe launches a 'clean-up' program, destroying shanty dwellings and illegal street stalls leaving about 700,000 people homeless."},
+			"2006":{"imglink":"img/zimbabwe/2006.png","text":"Economic crisis worsens as inflation exceeds 1,000%. New banknotes are introduced with three places deleted from their values in an attempt to help curb hyperinflation."},
+			"2007":{"imglink":"img/zimbabwe/2007.jpeg","text":"Demonstrations and rallies are banned. The government warns of power cuts for up to 20 hours a day while electricity is diverted towards agriculture."},
+			"2008":{"imglink":"img/zimbabwe/2008.jpg","text":"UN Security Council imposes sanctions on Zimbabwe. Mugabe declared the winner of elections as Tsvangirai pulls out due to intimidation. Zimbabwe declares a national emergency as cholera epidemic casues collapse of the health care system."},
+			"2009":{"imglink":"img/zimbabwe/2009.jpg","text":"Tsvangirai sworn in as prime-minister after he and Mugabe sign power-sharing agreement. Retail prices fall, constitutional review begins, and Tsvangirai tours Europe and US to revive foreign relations and drive donor support."},
+			"2010":{"imglink":"img/zimbabwe/2010.jpg","text":"Zimbabwe's High Court rules in favor of Mugabe's land-reform program while new 'indigenisation' law forces foreign-owned businesses to sell majority stake to locals. Zimbabwe resumes diamond sales at the Marange diamond fields amid allegations 'blood diamonds' and human rights abuses."},
+			"2011":{"imglink":"img/zimbabwe/2011.jpg","text":"The Kimberly Process (diamond industry regulator) lifts the export ban from two of Zimbabwe's Marange fields. Mugabe condemns his power-sharing agreement with Tsvangirai and says he will run in the next election."},
+			"2012":{"imglink":"img/zimbabwe/2012.jpg","text":"European Union eases sanctions while maintaining asset freeze and travel restrictions for President Mugabe. Political violence on the rise and repressive structures reappear in preparation for 2013 elections."},
+			"2013":{"imglink":"img/zimbabwe/2013.jpg","text":"Talks involving President Mugabe and Prime Minister Tsvangirai result in the approval of a new constitution. Mugabe is fraudulently elected for his 7th term in office and his party receives 3/4 of the seats in parliament."},
+			};
+            
 
    } ])
 
